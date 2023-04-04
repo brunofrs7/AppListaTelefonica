0e5b8c660388b23c4884d471e10f70c7e1aab831
Bruno Santos <brunofrs7@gmail.com>
2023-04-04 16:55:10 +0100

    Configuração do Gradle; Assets importados; Criação das Activity e colocação em packages; Criação do SplashScreenActivity

---
+++ app/src/main/java/com/example/listatelefonica/ui/SelecionarImagemContactoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.listatelefonica.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.listatelefonica.R
+
+class SelecionarImagemContactoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selecionar_imagem_contacto)
+    }
+}
