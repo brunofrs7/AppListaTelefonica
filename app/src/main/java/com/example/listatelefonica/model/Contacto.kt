7d79bb9eb9e42a941e4786addc4c833b2a344376
Bruno Santos <brunofrs7@gmail.com>
2023-04-04 17:42:37 +0100

    ROOM implementado

--- app/src/main/java/com/example/listatelefonica/model/Contacto.kt
+++ app/src/main/java/com/example/listatelefonica/model/Contacto.kt
@@ -1,6 +1,11 @@
 package com.example.listatelefonica.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Contacto")
 data class Contacto(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val nome: String = "",
     val endereco: String = "",
